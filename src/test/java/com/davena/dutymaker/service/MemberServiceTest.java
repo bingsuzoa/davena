74e62655e88d383c89388c0b07f30dae410736e9
bingsuzoa <zxc_777@naver.com>
2025-08-20 11:21:05 +0900

    charge 적용, 우선순위 적용 테스트 완료

--- src/test/java/com/davena/dutymaker/service/MemberServiceTest.java
+++ src/test/java/com/davena/dutymaker/service/MemberServiceTest.java
@@ -1,14 +1,13 @@
 package com.davena.dutymaker.service;
 
+import com.davena.dutymaker.api.dto.member.ChargeBox;
+import com.davena.dutymaker.api.dto.member.ChargeRequest;
 import com.davena.dutymaker.domain.organization.Hospital;
 import com.davena.dutymaker.domain.organization.SkillGrade;
-import com.davena.dutymaker.domain.organization.Team;
+import com.davena.dutymaker.domain.organization.team.Team;
 import com.davena.dutymaker.domain.organization.Ward;
 import com.davena.dutymaker.domain.organization.member.Member;
-import com.davena.dutymaker.repository.MemberRepository;
-import com.davena.dutymaker.repository.SkillGradeRepository;
-import com.davena.dutymaker.repository.TeamRepository;
-import com.davena.dutymaker.repository.WardRepository;
+import com.davena.dutymaker.repository.*;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -16,11 +15,17 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.context.annotation.Import;
+import org.springframework.test.context.ActiveProfiles;
+import org.testcontainers.shaded.org.checkerframework.checker.units.qual.C;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @DataJpaTest
 @Import({
         MemberService.class
 })
+@ActiveProfiles("test")
 public class MemberServiceTest {
 
     @Autowired
@@ -33,6 +38,10 @@ public class MemberServiceTest {
     private TeamRepository teamRepository;
     @Autowired
     private SkillGradeRepository skillGradeRepository;
+    @Autowired
+    private HospitalRepository hospitalRepository;
+    @Autowired
+    private SkillGradeRepository gradeRepository;
     @Autowired
     private MemberService memberService;
 
@@ -48,6 +57,8 @@ public class MemberServiceTest {
         return wardRepository.save(new Ward(hospital, supervisor, "외상병동"));
     }
 
+    /// //// 해피 테스트
+
     @Test
     @DisplayName("Member가 Ward 소속될 때 테스트")
     void Member가_ward_소속될_때_테스트() {
@@ -58,5 +69,131 @@ public class MemberServiceTest {
         Assertions.assertEquals(updatedMember.getWard().getName(), "외상병동");
         Assertions.assertEquals(updatedMember.getTeam().getName(), Team.DEFAULT_TEAM_NAME);
         Assertions.assertEquals(updatedMember.getSkillGrade().getName(), SkillGrade.DEFAULT_SKILL_GRADE);
+    }
+
+    @Test
+    @DisplayName("차지 여부 정하기 위해 멤버들 리스트 얻는 테스트")
+    void 차지_여부_결정_위한_멤버들_리스트_얻는_테스트() {
+        Hospital hospital = hospitalRepository.save(new Hospital());
+        Member supervisor = memberRepository.save(new Member("김간호", "김간호", "01011112222", "1234"));
+        Ward ward = wardRepository.save(new Ward(hospital, supervisor, "외상 병동"));
+
+        Team defaultTeam = teamRepository.findByWardIdAndIsDefaultTrue(ward.getId()).orElseThrow();
+        SkillGrade defaultGrade = gradeRepository.findByWardIdAndIsDefaultTrue(ward.getId()).orElseThrow();
+
+        Team bTeam = teamRepository.save(new Team(ward, "b팀"));
+
+        Member member1 = memberRepository.save(new Member("박간호", "박간호", "01011112223", "1234"));
+        member1.joinWard(ward, defaultTeam, defaultGrade);
+
+        Member member2 = memberRepository.save(new Member("최간호", "최간호", "01011112224", "1234"));
+        member2.joinWard(ward, bTeam, defaultGrade);
+
+        Member member3 = memberRepository.save(new Member("유간호", "유간호", "01011112225", "1234"));
+        member3.joinWard(ward, bTeam, defaultGrade);
+
+        Map<Long, ChargeBox> chargeBox = new HashMap<>();
+        chargeBox.put(member1.getId(), new ChargeBox(defaultTeam.getId(), defaultTeam.getName(), member1.getName(), true, 1));
+        chargeBox.put(member2.getId(), new ChargeBox(bTeam.getId(), bTeam.getName(), member2.getName(), true, 1));
+        ChargeRequest chargeRequest = new ChargeRequest(chargeBox);
+        memberService.updateChargeOfMember(chargeRequest);
+
+        Member chargeMember = memberRepository.findById(member1.getId()).orElseThrow();
+        Assertions.assertTrue(chargeMember.isCharge());
+        Assertions.assertEquals(chargeMember.getRank(), 1);
+    }
+
+    /// ///예외 테스트
+    @Test
+    @DisplayName("차지인데 Rank가 음수 이면 예외")
+    void 차지인데_Rank_음수이면_예외() {
+        Hospital hospital = hospitalRepository.save(new Hospital());
+        Member supervisor = memberRepository.save(new Member("김간호", "김간호", "01011112222", "1234"));
+        Ward ward = wardRepository.save(new Ward(hospital, supervisor, "외상 병동"));
+
+        Team defaultTeam = teamRepository.findByWardIdAndIsDefaultTrue(ward.getId()).orElseThrow();
+        SkillGrade defaultGrade = gradeRepository.findByWardIdAndIsDefaultTrue(ward.getId()).orElseThrow();
+
+        Team bTeam = teamRepository.save(new Team(ward, "b팀"));
+
+        Member member1 = memberRepository.save(new Member("박간호", "박간호", "01011112223", "1234"));
+        member1.joinWard(ward, defaultTeam, defaultGrade);
+
+        Member member2 = memberRepository.save(new Member("최간호", "최간호", "01011112224", "1234"));
+        member2.joinWard(ward, bTeam, defaultGrade);
+
+        Member member3 = memberRepository.save(new Member("유간호", "유간호", "01011112225", "1234"));
+        member3.joinWard(ward, bTeam, defaultGrade);
+
+        Map<Long, ChargeBox> chargeBox = new HashMap<>();
+        chargeBox.put(member1.getId(), new ChargeBox(defaultTeam.getId(), defaultTeam.getName(), member1.getName(), true, -1));
+        chargeBox.put(member2.getId(), new ChargeBox(bTeam.getId(), bTeam.getName(), member2.getName(), true, 1));
+        ChargeRequest chargeRequest = new ChargeRequest(chargeBox);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            memberService.updateChargeOfMember(chargeRequest);
+        });
+    }
+
+    @Test
+    @DisplayName("차지인데 Rank가 0 이면 예외")
+    void 차지인데_Rank_0이면_예외() {
+        Hospital hospital = hospitalRepository.save(new Hospital());
+        Member supervisor = memberRepository.save(new Member("김간호", "김간호", "01011112222", "1234"));
+        Ward ward = wardRepository.save(new Ward(hospital, supervisor, "외상 병동"));
+
+        Team defaultTeam = teamRepository.findByWardIdAndIsDefaultTrue(ward.getId()).orElseThrow();
+        SkillGrade defaultGrade = gradeRepository.findByWardIdAndIsDefaultTrue(ward.getId()).orElseThrow();
+
+        Team bTeam = teamRepository.save(new Team(ward, "b팀"));
+
+        Member member1 = memberRepository.save(new Member("박간호", "박간호", "01011112223", "1234"));
+        member1.joinWard(ward, defaultTeam, defaultGrade);
+
+        Member member2 = memberRepository.save(new Member("최간호", "최간호", "01011112224", "1234"));
+        member2.joinWard(ward, bTeam, defaultGrade);
+
+        Member member3 = memberRepository.save(new Member("유간호", "유간호", "01011112225", "1234"));
+        member3.joinWard(ward, bTeam, defaultGrade);
+
+        Map<Long, ChargeBox> chargeBox = new HashMap<>();
+        chargeBox.put(member1.getId(), new ChargeBox(defaultTeam.getId(), defaultTeam.getName(), member1.getName(), true, 0));
+        chargeBox.put(member2.getId(), new ChargeBox(bTeam.getId(), bTeam.getName(), member2.getName(), true, 1));
+        ChargeRequest chargeRequest = new ChargeRequest(chargeBox);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            memberService.updateChargeOfMember(chargeRequest);
+        });
+    }
+
+    @Test
+    @DisplayName("차지인데 Rank가 MAX보다 초과이면 예외")
+    void 차지인데_Rank_MAX_초과이면_예외() {
+        Hospital hospital = hospitalRepository.save(new Hospital());
+        Member supervisor = memberRepository.save(new Member("김간호", "김간호", "01011112222", "1234"));
+        Ward ward = wardRepository.save(new Ward(hospital, supervisor, "외상 병동"));
+
+        Team defaultTeam = teamRepository.findByWardIdAndIsDefaultTrue(ward.getId()).orElseThrow();
+        SkillGrade defaultGrade = gradeRepository.findByWardIdAndIsDefaultTrue(ward.getId()).orElseThrow();
+
+        Team bTeam = teamRepository.save(new Team(ward, "b팀"));
+
+        Member member1 = memberRepository.save(new Member("박간호", "박간호", "01011112223", "1234"));
+        member1.joinWard(ward, defaultTeam, defaultGrade);
+
+        Member member2 = memberRepository.save(new Member("최간호", "최간호", "01011112224", "1234"));
+        member2.joinWard(ward, bTeam, defaultGrade);
+
+        Member member3 = memberRepository.save(new Member("유간호", "유간호", "01011112225", "1234"));
+        member3.joinWard(ward, bTeam, defaultGrade);
+
+        Map<Long, ChargeBox> chargeBox = new HashMap<>();
+        chargeBox.put(member1.getId(), new ChargeBox(defaultTeam.getId(), defaultTeam.getName(), member1.getName(), true, Member.MAX_RANKING + 1));
+        chargeBox.put(member2.getId(), new ChargeBox(bTeam.getId(), bTeam.getName(), member2.getName(), true, 1));
+        ChargeRequest chargeRequest = new ChargeRequest(chargeBox);
+
+        Assertions.assertThrows(IllegalArgumentException.class, () -> {
+            memberService.updateChargeOfMember(chargeRequest);
+        });
     }
 }
--- src/test/java/com/davena/dutymaker/service/WardServiceTest.java
+++ src/test/java/com/davena/dutymaker/service/WardServiceTest.java
@@ -1,14 +1,14 @@
 package com.davena.dutymaker.service;
 
+import com.davena.dutymaker.api.dto.member.ChargeRequest;
 import com.davena.dutymaker.api.dto.ward.WardRequest;
 import com.davena.dutymaker.domain.organization.Hospital;
+import com.davena.dutymaker.domain.organization.SkillGrade;
 import com.davena.dutymaker.domain.organization.Ward;
 import com.davena.dutymaker.domain.organization.member.Member;
+import com.davena.dutymaker.domain.organization.team.Team;
 import com.davena.dutymaker.domain.shiftRequirement.ShiftType;
-import com.davena.dutymaker.repository.HospitalRepository;
-import com.davena.dutymaker.repository.MemberRepository;
-import com.davena.dutymaker.repository.ShiftTypeRepository;
-import com.davena.dutymaker.repository.WardRepository;
+import com.davena.dutymaker.repository.*;
 import jakarta.persistence.EntityManager;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.DisplayName;
@@ -37,6 +37,10 @@ public class WardServiceTest {
     @Autowired
     private ShiftTypeRepository shiftRepository;
     @Autowired
+    private TeamRepository teamRepository;
+    @Autowired
+    private SkillGradeRepository gradeRepository;
+    @Autowired
     private WardService wardService;
 
     @Test
@@ -47,5 +51,32 @@ public class WardServiceTest {
         WardRequest wardRequest = new WardRequest(hospital.getId(), "외상 병동");
         Ward ward = wardService.createWardAndOffType(supervisor.getId(), wardRequest);
         Assertions.assertNotNull(shiftRepository.findByWardIdAndName(ward.getId(), ShiftType.OFF));
+    }
+
+    @Test
+    @DisplayName("차지 여부 정하기 위해 멤버들 리스트 얻는 테스트")
+    void 차지_여부_결정_위한_멤버들_리스트_얻는_테스트() {
+        Hospital hospital = hospitalRepository.save(new Hospital());
+        Member supervisor = memberRepository.save(new Member("김간호", "김간호", "01011112222", "1234"));
+        Ward ward = wardRepository.save(new Ward(hospital, supervisor, "외상 병동"));
+
+        Team defaultTeam = teamRepository.findByWardIdAndIsDefaultTrue(ward.getId()).orElseThrow();
+        SkillGrade defaultGrade = gradeRepository.findByWardIdAndIsDefaultTrue(ward.getId()).orElseThrow();
+
+        Team bTeam = teamRepository.save(new Team(ward, "b팀"));
+
+        Member member1 = memberRepository.save(new Member("박간호", "박간호", "01011112223", "1234"));
+        member1.joinWard(ward, defaultTeam, defaultGrade);
+
+        Member member2 = memberRepository.save(new Member("최간호", "최간호", "01011112224", "1234"));
+        member2.joinWard(ward, bTeam, defaultGrade);
+
+        Member member3 = memberRepository.save(new Member("유간호", "유간호", "01011112225", "1234"));
+        member3.joinWard(ward, bTeam, defaultGrade);
+
+        ChargeRequest chargeRequest = wardService.getMembersForCharge(ward.getId());
+        Assertions.assertEquals(chargeRequest.chargeMap().size(), 3);
+        Assertions.assertEquals(chargeRequest.chargeMap().get(member1.getId()).isCharge(), false);
+        Assertions.assertEquals(chargeRequest.chargeMap().get(member1.getId()).ranking(), Member.MAX_RANKING);
     }
 }
